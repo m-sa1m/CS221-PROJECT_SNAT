dc1b3559719d10d8f459e33addc43db7a96e25ed
mhariszaffar <hariszafar102@gmail.com>
2025-10-19 03:06:21 +0500

    Stored objects in linked list and used a loop to dispaly output in main.cpp

--- Code/main.cpp
+++ Code/main.cpp
@@ -13,13 +13,37 @@ int main()
     System* systems[] = {&sys1, &sys2, &sys3};
     int count = 3;
 
-    // to calculate risks
+
+    //to calculate risks
     cout << "\nCalculating Risk score...\n"; 
     for(int i = 0; i < count; i++) {
         calculateRisk(systems[i]);
     }
     
+
     // to sort by risks
     sortSystems(systems, count);
 
+
+    //ouput
+    cout << "\n=== RANKED SYSTEM ===\n";
+    for(int i = 0; i < count; i++) {
+        string level = (systems[i]->riskScore < 50) ? "LOW" : 
+                      (systems[i]->riskScore < 80) ? "MEDIUM" : "HIGH";
+        cout << i+1 << ". " << systems[i]->name << " - " 
+             << systems[i]->riskScore << "/100 (" << level << ")\n";
+    }
+    
+
+   // storing in linked-list 
+    SystemList network;
+    network.addSystem(&sys1);
+    network.addSystem(&sys2);
+    network.addSystem(&sys3);
+    
+    cout << "\nLinked List: 3 systems stored dynamically\n";
+    
+    return 0;
 }
+
+
