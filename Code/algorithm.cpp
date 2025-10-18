1f82e4072474a7003167318e81b7e09dedae8f33
m-sa1m <saimwaqas100@gmail.com>
2025-10-19 02:35:17 +0500

    Added Selection Sort For Ranking

--- Code/algorithm.cpp
+++ Code/algorithm.cpp
@@ -14,3 +14,19 @@ int calculateRisk(System* sys) {
     sys->riskScore = (score > 100) ? 100 : score;
     return sys->riskScore;
 }
+
+//  Selection Sort For Ranking
+void sortSystems(System* systems[], int n) {
+    for(int i = 0; i < n-1; i++) {
+        int maxIndex = i;
+        for(int j = i+1; j < n; j++) {
+            if(systems[j]->riskScore > systems[maxIndex]->riskScore)
+                maxIndex = j;
+        }
+        if(maxIndex != i) {
+            System* temp = systems[i];
+            systems[i] = systems[maxIndex];
+            systems[maxIndex] = temp;
+        }
+    }
+}
