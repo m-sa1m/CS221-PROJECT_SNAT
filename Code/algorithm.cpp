9fe5a90dc1074e47c9e80570e58e440e2f78b9d0
ibrahim-gulzar <ibrahimgulzar2020@gmail.com>
2025-10-19 19:07:10 +0500

    .

--- Code/algorithm.cpp
+++ Code/algorithm.cpp
@@ -4,34 +4,34 @@ using namespace std;
 #include "structure.h"
 
 
-int calculateRisk(System* sys) {
+int calculate_risk(System* sys) {
     int score = 0;
     
     // Password risk
-    score += (10 - sys->passwordStrength) * 3;
+    score += (10 - sys->password_strength) * 3;
     
     // Open ports risk
-    score += sys->openPorts * 8;
+    score += sys->open_ports * 8;
     
     // Firewall risk
-    if(!sys->firewallOn) score += 25;
+    if(!sys->firewalls) score += 25;
     
-    sys->riskScore = (score > 100) ? 100 : score;
-    return sys->riskScore;
+    sys->risk_score = (score > 100) ? 100 : score;
+    return sys->risk_score;
 }
 
 //  Selection Sort For Ranking
-void sortSystems(System* systems[], int n) {
+void sort_systems(System* systems[], int n) {
     for(int i = 0; i < n-1; i++) {
-        int maxIndex = i;
+        int max_index = i;
         for(int j = i+1; j < n; j++) {
-            if(systems[j]->riskScore > systems[maxIndex]->riskScore)
+            if(systems[j]->risk_score > systems[maxIndex]->risk_score)
                 maxIndex = j;
         }
-        if(maxIndex != i) {
+        if(max_index != i) {
             System* temp = systems[i];
-            systems[i] = systems[maxIndex];
-            systems[maxIndex] = temp;
+            systems[i] = systems[max_index];
+            systems[max_index] = temp;
         }
     }
 }
