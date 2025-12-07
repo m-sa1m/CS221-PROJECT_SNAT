c22d63700b2e8ea06cf462c25547c73d7430bea3
m-sa1m <saimwaqas100@gmail.com>
2025-12-07 16:29:27 +0500

    System structure made in structure.h

--- Deliverable 3/Code/structure.h
+++ Deliverable 3/Code/structure.h
@@ -5,7 +5,7 @@
 #include <string>
 using namespace std;
 
-// STEP 1: Enhanced System structure (combining both versions)
+// System structure
 struct System {
     // Basic info
     string name;
@@ -27,96 +27,3 @@ struct System {
     int vuln_count;
     bool is_vulnerable;
 };
-
-// STEP 2: Node for Circular Doubly Linked List
-struct Node {
-    System* data;
-    Node* next;
-    Node* prev;
-};
-
-// STEP 3: SystemList class (Circular Doubly Linked List)
-class SystemList {
-private:
-    Node* head;
-    int count;
-    
-public:
-    SystemList();
-    ~SystemList();
-    
-    // Core operations
-    void add_system(System* sys);
-    bool delete_system(string name);
-    System* search_system(string name);
-    int get_count();
-    
-    // Display operations
-    void display_all();
-    void display_high_risk();
-    void display_by_ip(string ip);
-    
-    // Traversal
-    Node* get_head();
-    void traverse_forward();
-    void traverse_backward();
-};
-
-// STEP 4: Stack for Task Management (LIFO)
-struct TaskStack {
-    string tasks[20];
-    int top;
-    
-    TaskStack();
-    void push(string task);
-    string pop();
-    bool is_empty();
-    bool is_full();
-    int size();
-};
-
-// STEP 5: Queue for Scanning Tasks (FIFO)
-struct ScanQueue {
-    string tasks[20];
-    int front;
-    int rear;
-    int count;
-    
-    ScanQueue();
-    void enqueue(string task);
-    string dequeue();
-    bool is_empty();
-    bool is_full();
-    int size();
-};
-
-// STEP 6: Binary Search Tree for Risk-based Organization
-struct BSTNode {
-    System* data;
-    BSTNode* left;
-    BSTNode* right;
-};
-
-class RiskBST {
-private:
-    BSTNode* root;
-    
-    BSTNode* insert_helper(BSTNode* node, System* sys);
-    void inorder_helper(BSTNode* node);
-    void preorder_helper(BSTNode* node);
-    void postorder_helper(BSTNode* node);
-    BSTNode* search_helper(BSTNode* node, int risk_score);
-    void destroy_tree(BSTNode* node);
-    
-public:
-    RiskBST();
-    ~RiskBST();
-    
-    void insert(System* sys);
-    void inorder_traversal();
-    void preorder_traversal();
-    void postorder_traversal();
-    System* search_by_risk(int risk_score);
-};
-
-#endif
