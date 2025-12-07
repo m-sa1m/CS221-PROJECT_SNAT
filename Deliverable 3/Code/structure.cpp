9735ccb6c8a3ad31012d5386bd70179887d6e902
m-sa1m <saimwaqas100@gmail.com>
2025-12-07 16:48:34 +0500

    implementation of circular double linked list in structure .cpp

--- Deliverable 3/Code/structure.cpp
+++ Deliverable 3/Code/structure.cpp
@@ -1,19 +1,48 @@
 #include <iostream>
-using namespace std;
 #include "structure.h"
+using namespace std;
 
+// SystemList Implementation
 
-int risk_weight[3]={30,25,35};
-
-SystemList::SystemList()
-{
+// Constructor - Initialize empty list
+SystemList::SystemList() {
     head = NULL;
+    count = 0;
 }
 
-void SystemList::add_system(System * sys)
-{
-    Node *newNode= new Node();
-    newNode->data= sys;
-    newNode->next= head;
-    head = newNode;
+// Destructor - Free all memory
+SystemList::~SystemList() {
+    if (!head) return;
+    
+    Node* current = head;
+    Node* start = head;
+    
+    // Traverse and delete all nodes
+    do {
+        Node* temp = current;
+        current = current->next;
+        delete temp->data;  // Delete system data
+        delete temp;        // Delete node
+    } while (current != start);
+}
+
+// Add system to circular doubly linked list
+void SystemList::add_system(System* sys) {
+    Node* newNode = new Node();
+    newNode->data = sys;
+    
+    if (!head) {
+        // First node - point to itself
+        head = newNode;
+        head->next = head;
+        head->prev = head;
+    } else {
+        // Insert at end (before head)
+        Node* last = head->prev;
+        last->next = newNode;
+        newNode->prev = last;
+        newNode->next = head;
+        head->prev = newNode;
+    }
+    count++;
 }
