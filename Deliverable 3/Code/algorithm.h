cd3904ced9da308c8a9eb22b86811f31a9d96797
m-sa1m <saimwaqas100@gmail.com>
2025-12-07 17:07:34 +0500

    Searching algos scanning and vulnerability detection performance analysis

--- Deliverable 3/Code/algorithm.h
+++ Deliverable 3/Code/algorithm.h
@@ -19,5 +19,23 @@ void insertion_sort(System* systems[], int n);
 void quick_sort(System* systems[], int low, int high);
 void merge_sort(System* systems[], int left, int right);
 
+// Helper functions
+int partition(System* systems[], int low, int high);
+void merge(System* systems[], int left, int mid, int right);
+
+// Searching Algorithms
+System* linear_search(System* systems[], int n, string name);
+System* binary_search_by_risk(System* systems[], int n, int risk_score);
+
+
+// Port Scanning & Vulnerability Detection
+void scan_ports(System* sys);
+void check_vulnerabilities(System* sys);
+
+// Performance Analysis
+void compare_sorting_algorithms(System* systems[], int n);
+double measure_sorting_time(void (*sort_func)(System*[], int), System* systems[], int n);
+
+
 
 #endif
