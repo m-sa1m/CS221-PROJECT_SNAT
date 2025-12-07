b8280253b5ee1df7a2f9e4e847e8a42709b1d2e0
m-sa1m <saimwaqas100@gmail.com>
2025-12-07 17:33:15 +0500

    Risk CAlculation algo implemented

--- Deliverable 3/Code/algorithm.cpp
+++ Deliverable 3/Code/algorithm.cpp
@@ -1,37 +1,50 @@
-cd#include <iostream>
-using namespace std;
-
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <chrono>
 #include "algorithm.h"
 #include "structure.h"
+using namespace std;
+
+// Risk weights array
+int risk_weights[5] = {25, 30, 15, 20, 10}; // Password, Ports, Encryption, Malware, Firewall
+
+// ============================================
+// SECTION 1: Risk Calculation
+// ============================================
 
 int calculate_risk(System* sys) {
     int score = 0;
     
-    // Password risk
-    score += (10 - sys->password_strength) * 3;
+    // Password risk (weaker password = higher risk)
+    score += (10 - sys->password_strength) * risk_weights[0] / 10;
     
     // Open ports risk
-    score += sys->open_ports * 8;
+    score += sys->open_ports * risk_weights[1] / 2;
+    
+    // Encryption risk
+    if (!sys->encryption_enabled) score += risk_weights[2];
+    
+    // Malware risk
+    if (sys->malware_detected) score += risk_weights[3];
     
     // Firewall risk
-    if(!sys->firewalls) score += 25;
+    if (!sys->firewalls) score += risk_weights[4];
     
+    // Cap at 100
     sys->risk_score = (score > 100) ? 100 : score;
     return sys->risk_score;
 }
 
-//  Selection Sort For Ranking
-void sort_systems(System* systems[], int n) {
-    for(int i = 0; i < n-1; i++) {
-        int max_index = i;
-        for(int j = i+1; j < n; j++) {
-            if(systems[j]->risk_score > systems[max_index]->risk_score)
-                max_index = j;
-        }
-        if(max_index != i) {
-            System* temp = systems[i];
-            systems[i] = systems[max_index];
-            systems[max_index] = temp;
-        }
-    }
+void calculate_all_risks(SystemList& list) {
+    Node* head = list.get_head();
+    if (!head) return;
+    
+    Node* temp = head;
+    do {
+        calculate_risk(temp->data);
+        temp = temp->next;
+    } while (temp != head);
 }
+
+
